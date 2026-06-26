55b6f3a4dd5f8e8e938870962e90fa2b573169c6
arafat248 <aab.arafat248@gmail.com>
2026-06-26 22:55:57 +0600

    add jobapp views

---
+++ apps/jobapp/views/__init__.py
@@ -0,0 +1,12 @@
+from .employee import ApplyJobView, DeleteBookmarkView, JobBookmarkView
+from .employer import (
+    AllApplicantsView,
+    ApplicantDetailsView,
+    CreateJobView,
+    DeleteJobView,
+    JobEditView,
+    MakeCompleteJobView,
+    UpdateApplicantStatusView,
+)
+from .public import JobListView, SearchResultView, SingleJobView, home_view
+from .shared import dashboard_view
